6014f4f6809e752603656d3374a81c6ecf973734
HaMinhTri1 <156415534+HaMinhTri1@users.noreply.github.com>
2024-03-21 09:25:54 +0700

    C1_5.py

--- TH/C1_5.py
+++ TH/C1_5.py
@@ -1,2 +1,7 @@
-def TongUocNguyenDuong(n):
-    
+print("Nhap N: ")
+n = int(input())
+sum = 0
+for i in range(1, n+1):
+    if (n % i == 0):
+        sum += i
+print("Tổng tất cả các ước số ", n, " là: ", sum)
